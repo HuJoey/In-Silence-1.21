7cd507ab92f8c52c8da08cae28df2b7ba8cda4d2
HuJoey <hujoeofficial@gmail.com>
2024-09-30 11:19:01 -0400

    working wheel HUD

--- src/main/java/net/hujoe/insilence/InsilenceClient.java
+++ src/main/java/net/hujoe/insilence/InsilenceClient.java
@@ -3,18 +3,26 @@ package net.hujoe.insilence;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.MathHelper;
 
 public class InsilenceClient implements ClientModInitializer {
-    private static final net.minecraft.util.Identifier RAKE_WHEEL = Identifier.of(Insilence.MOD_ID,"textures/gui/wheel.png");
+    private static final net.minecraft.util.Identifier RAKE_WHEEL = Identifier.of(Insilence.MOD_ID,"textures/gui/sprites/wheel.png");
     @Override
     public void onInitializeClient(){
+        // renders the rake wheel texture above the hotbar
         HudRenderCallback.EVENT.register((drawContext, tickDeltaManager) -> {
-            int i = drawContext.getScaledWindowWidth() / 2;
-            drawContext.drawGuiTexture(RAKE_WHEEL, i - 91, drawContext.getScaledWindowHeight() - 22, 182, 182);
+            MinecraftClient client = MinecraftClient.getInstance();
+            int x = client.getWindow().getScaledWidth() / 2;
+            int y = client.getWindow().getScaledHeight();
+            RenderSystem.setShader(GameRenderer::getRenderTypeTextSeeThroughProgram);
+            RenderSystem.setShaderColor(1.0F,1.0F,1.0F,1.0F);
+            RenderSystem.enableBlend(); // allows wheel to be transparent
+            drawContext.drawTexture(RAKE_WHEEL, x - 78, y - 110,0, 0, 156, 64, 156, 64); // draws the rake wheel texture
+            RenderSystem.disableBlend(); // prevents transparency issue when hitting esc
         });
     }
 }
