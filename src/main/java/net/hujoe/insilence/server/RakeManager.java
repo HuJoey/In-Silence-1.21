ece1430cf1664c26067175b1ccf449c37437633e
HuJoey <hujoeofficial@gmail.com>
2024-10-05 01:13:53 -0400

    Class to track current Rakes

---
+++ src/main/java/net/hujoe/insilence/server/RakeManager.java
@@ -0,0 +1,36 @@
+package net.hujoe.insilence.server;
+
+import java.util.ArrayList;
+
+public class RakeManager {
+    private ArrayList<String> usernames;
+    public RakeManager(){
+        usernames = new ArrayList<String>();
+    }
+
+    public void addUser(String username){
+        usernames.add(username);
+    }
+
+    public void removeUser(String username){
+        usernames.remove(username);
+    }
+
+    public boolean isRake(String username){
+        return usernames.contains(username);
+    }
+
+    public void toggleUser(String username){
+        if (isRake(username)){
+            removeUser(username);
+        } else {
+            addUser(username);
+        }
+    }
+
+    private static final RakeManager rakeManager = new RakeManager();
+
+    public static RakeManager getRakeManager(){
+        return rakeManager;
+    }
+}
