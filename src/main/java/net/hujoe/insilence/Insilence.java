547313735a76523997f8286ff5bf48fbee7e04bb
HuJoey <hujoeofficial@gmail.com>
2024-12-15 23:16:27 -0500

    Moved Commands to Custom Class

--- src/main/java/net/hujoe/insilence/Insilence.java
+++ src/main/java/net/hujoe/insilence/Insilence.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.hujoe.insilence.block.ModBlocks;
 import net.hujoe.insilence.block.entity.ModBlockEntities;
+import net.hujoe.insilence.commands.ModCommands;
 import net.hujoe.insilence.entity.ModEntities;
 import net.hujoe.insilence.entity.custom.SoundEntity;
 import net.hujoe.insilence.item.ModItems;
@@ -73,44 +74,7 @@ public class Insilence implements ModInitializer {
 		ModBlocks.registerModBlocks();
 		ModBlockEntities.registerModBlockEntities();
 		ModSounds.registerModSounds();
-
-		CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> dispatcher.register(literal("rake")
-				.requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
-				.executes(commandContext -> {
-					ServerCommandSource source = commandContext.getSource();
-					Entity sender = source.getEntity();
-					if (sender != null) {
-						RakeManager.getRakeManager().toggleUser(sender.getNameForScoreboard(), sender.getWorld());
-					}
-					commandContext.getSource().sendFeedback(() -> Text.literal("You Toggled Rake"), false);
-					return 1;
-				}))));
-
-		CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> dispatcher.register(literal("playbirdsound")
-				.requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
-				.executes(commandContext -> {
-					ServerCommandSource source = commandContext.getSource();
-					Vec3d pos = source.getPosition();
-					World world = source.getWorld();
-
-					double rand = Math.random(); // limited range of 8.5 blocks in order to hear this sound
-					if (rand < 0.25) {
-						world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), ModSounds.CROW_01_EVENT, SoundCategory.AMBIENT, 0.25F, 1);
-					} else if (rand < 0.5){
-						world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), ModSounds.CROW_02_EVENT, SoundCategory.AMBIENT, 0.25F, 1);
-					} else if (rand < 0.75){
-						world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), ModSounds.OWL_HOOT_01_EVENT, SoundCategory.AMBIENT, 0.25F, 1);
-					} else {
-						world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), ModSounds.OWL_HOOT_02_EVENT, SoundCategory.AMBIENT, 0.25F, 1);
-
-					}
-
-					SoundEntity soundEntity = new SoundEntity(ModEntities.SOUNDENTITY, world);
-					soundEntity.setStrength(50);
-					soundEntity.setPosition(pos);
-					world.spawnEntity(soundEntity);
-					return 1;
-				}))));
+		ModCommands.registerModCommands();
 
 		PayloadTypeRegistry.playS2C().register(RakeUpdatePayload.ID, RakeUpdatePayload.CODEC);
 		PayloadTypeRegistry.playS2C().register(RakeListReceivePayload.ID, RakeListReceivePayload.CODEC);
---
+++ src/main/java/net/hujoe/insilence/commands/ModCommands.java
@@ -0,0 +1,77 @@
+package net.hujoe.insilence.commands;
+
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.hujoe.insilence.entity.ModEntities;
+import net.hujoe.insilence.entity.custom.SoundEntity;
+import net.hujoe.insilence.server.RakeManager;
+import net.hujoe.insilence.sound.ModSounds;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class ModCommands {
+    public static void registerModCommands(){
+        CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> dispatcher.register(literal("rake")
+                .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
+                .executes(commandContext -> {
+                    ServerCommandSource source = commandContext.getSource();
+                    Entity sender = source.getEntity();
+                    if (sender != null) {
+                        RakeManager.getRakeManager().toggleUser(sender.getNameForScoreboard(), sender.getWorld());
+                    }
+                    commandContext.getSource().sendFeedback(() -> Text.literal("You Toggled Rake"), false);
+                    return 1;
+                }))));
+
+        CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> dispatcher.register(literal("playbirdsound")
+                .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
+                .executes(commandContext -> {
+                    ServerCommandSource source = commandContext.getSource();
+                    Vec3d pos = source.getPosition();
+                    World world = source.getWorld();
+
+                    double rand = Math.random(); // limited range of 8.5 blocks in order to hear this sound
+                    if (rand < 0.25) {
+                        world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), ModSounds.CROW_01_EVENT, SoundCategory.AMBIENT, 0.25F, 1);
+                    } else if (rand < 0.5){
+                        world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), ModSounds.CROW_02_EVENT, SoundCategory.AMBIENT, 0.25F, 1);
+                    } else if (rand < 0.75){
+                        world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), ModSounds.OWL_HOOT_01_EVENT, SoundCategory.AMBIENT, 0.25F, 1);
+                    } else {
+                        world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), ModSounds.OWL_HOOT_02_EVENT, SoundCategory.AMBIENT, 0.25F, 1);
+
+                    }
+
+                    SoundEntity soundEntity = new SoundEntity(ModEntities.SOUNDENTITY, world);
+                    soundEntity.setStrength(50);
+                    soundEntity.setPosition(pos);
+                    world.spawnEntity(soundEntity);
+                    return 1;
+                }))));
+
+        CommandRegistrationCallback.EVENT.register(((dispatcher, registryAccess, environment) -> dispatcher.register(literal("playhumansound")
+                .requires(serverCommandSource -> serverCommandSource.hasPermissionLevel(2))
+                .executes(commandContext -> {
+                    ServerCommandSource source = commandContext.getSource();
+                    Vec3d pos = source.getPosition();
+                    World world = source.getWorld();
+                    Entity entity = source.getEntity();
+
+                    if (entity instanceof PlayerEntity){
+                        ((PlayerEntity) entity).playSoundToPlayer();
+                    }
+
+                    SoundEntity soundEntity = new SoundEntity(ModEntities.SOUNDENTITY, world);
+                    soundEntity.setStrength(50);
+                    soundEntity.setPosition(pos);
+                    world.spawnEntity(soundEntity);
+                    return 1;
+                }))));
+    }
+}
